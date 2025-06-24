8f18d8ca0c546a580dac368bb883e23e68b43bd6
Radhika Sonagra <131811961+radhikasonagra@users.noreply.github.com>
2025-06-24 10:45:06 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Radhika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Radhika" => true
+
+let someNumber = 20
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
